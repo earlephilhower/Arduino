b8bff09b72d6c1f36a92d00a15a1fe15bb401909
Earle F. Philhower, III <earlephilhower@yahoo.com>
2020-07-09 15:14:12 -0700

    Fix publish build

--- package/deploy_package_index.sh
+++ package/deploy_package_index.sh
@@ -7,6 +7,8 @@ tag=`git describe --tags`
 
 cd $(dirname "$0")
 
+set -e # Abort with error if anything here does not go as expected!
+
 # Decrypt and install SSH private key.
 # "encrypted_xxx_key" and "encrypted_xxx_iv" are environment variables
 # known to Travis CI builds.
@@ -14,14 +16,18 @@ openssl enc -d -aes-256-cbc -K $GHKEY -iv $GHIV -in esp8266_github_io_deploy.enc
 eval "$(ssh-agent -s)"
 chmod 600 esp8266_github_io_deploy
 ssh-add esp8266_github_io_deploy
+mkdir -p ~/.ssh
+chmod go-w ~/.ssh
 echo -e "Host github.com\nStrictHostKeyChecking no\n" >> ~/.ssh/config
+chmod go-w  ~/.ssh/config
 
 # Clone the Github pages repository
 git clone git@github.com:earlephilhower/esp8266.github.io.git
 pushd esp8266.github.io
 
 # Update the package index
-cp ../versions/$tag/package_esp8266com_index.json stable/package_esp8266com_index.json
+ls -altr ../
+cp ../package/versions/*/package_esp8266com_index.json stable/package_esp8266com_index.json
 git add stable/package_esp8266com_index.json
 
 # Commit and push the changes
